2c96558760ef35763a5f7ae6200407edf22e292c
Felix Green <work@felix.green>
2021-05-17 00:17:36 -0400

    cleanup

--- src/lib/comps/Hero.svelte
+++ src/lib/comps/Hero.svelte
@@ -4,69 +4,75 @@
 	import Content from '@/lib/bonds/Content.svelte'
 	import Box from '@/lib/atoms/Box.svelte'
 	import Image from '@/lib/atoms/Image.svelte'
-	import Socials from '@/lib/comps/Socials.svelte'
 	import Inline from '@/lib/bonds/Inline.svelte'
 	import Stack from '@/lib/bonds/Stack.svelte'
 	import Text from '@/lib/bonds/Text.svelte'
-	import Link from '@/lib/bonds/Link.svelte'
 	import Hide from '@/lib/bonds/Hide.svelte'
-	import Button from '@/lib/bonds/Button.svelte'
 	import AddToChrome from '@/lib/comps/AddToChrome.svelte'
 
 	import ImgArtseeTextLarge from '@/assets/artsee_text_large.svg'
 	import ImgArtseeGoldLogo from '@/assets/artsee_gold_logo.svg'
 	import IconEth from '@/assets/icon_ethereum.svg'
-	import IconChrome from '@/assets/icon_chrome.svg'
 
 	const ss = stitch({
 		bg: '$grey200',
 		fontFamily: '$main',
-		// position: 'relative',
-		// zIndex: 100,
+		// py: '$12',
 	})
 
 	const ss1 = stitch({
-		width: '640px',
+		// width: '50%',
 		position: 'relative',
 		left: '$-12',
+		// top: '$-12',
+		// bottom: '$-12',
+		height: '$full',
+		display: 'flex',
+		alignItems: 'center',
+		// my: '$-12',
 
+		// '@initial': {
+		// width: '0px',
+		// },
+		// '@sm': {
+		// 	width: '320px',
+		// },
+		// '@md': {
+		// 	width: '100%',
+		// },
+		// '@lg': {
+		// 	width: '640px',
+		// },
+	})
+
+	const cssHero = {
+		py: '$12',
+		pr: '$12',
 		'@initial': {
-			width: '0px',
-		},
-		'@sm': {
-			width: '320px',
+			alignItems: 'center',
+			justifyContent: 'center',
+			textAlign: 'center',
 		},
 		'@md': {
-			width: '480px',
+			minWidth: '$sm',
+			alignItems: 'start',
+			textAlign: 'left',
 		},
 		'@lg': {
-			width: '640px',
+			minWidth: '$lg',
 		},
-		// bg: '$grey200',
-		// position: 'relative',
-		// zIndex: 100,
-	})
+	}
 </script>
 
 <Box cls={ss}>
-	<Hide below="sm">
-		<!-- <Box css={{ width: '60vw', position: 'absolute', left: '$-12' }}>
-			<Image
-				src={ImgArtseeTextLarge}
-				alt="artsee logo"
-				css={{ maxWidth: '$full', maxHeight: '$full' }}
-			/>
-		</Box> -->
-	</Hide>
 	<Content>
-		<Inline>
-
-			<Hide below="sm">
+		<Inline align="center">
+			<Hide below="md">
 				<Box cls={ss1}>
 					<Image src={ImgArtseeTextLarge} css={{ width: '$full' }} />
 				</Box>
 			</Hide>
-			<Stack align="leading" space="md">
+			<Stack align="leading" space="md" css={cssHero}>
 				<Image src={ImgArtseeGoldLogo} alt="artsee logo" />
 				<Text
 					css={{ color: '$gold', textTransform: 'uppercase', fontWeight: '$semi' }}
@@ -74,7 +80,7 @@
 					artsee for chrome
 				</Text>
 				<Text mdx="h5">bring NFTs into your daily life</Text>
-				<Text mdx="p">
+				<Text mdx="p" css={{ maxWidth: '$md' }}>
 					artsee uses your
 					<Image
 						src={IconEth}
