29d1d3c5be2fc667b62e7e0110eb9db97a1ab70a
Felix Green <work@felix.green>
2021-05-17 00:16:51 -0400

    refactor to own component

--- src/lib/comps/AddToChrome.svelte
+++ src/lib/comps/AddToChrome.svelte
@@ -1,14 +1,18 @@
 <script lang="ts">
 	import Image from '@/lib/atoms/Image.svelte'
 	import Inline from '@/lib/bonds/Inline.svelte'
-
+	import Link from '@/lib/bonds/Link.svelte'
 	import Button from '@/lib/bonds/Button.svelte'
 	import IconChrome from '@/assets/icon_chrome.svg'
+
+	import { chrome } from '@/lib/datum/socials'
 </script>
 
-<Button>
-	<Inline alignV="center" space="sm">
-		<Image src={IconChrome} />
-		add to chrome
-	</Inline>
-</Button>
+<Link newTab url={chrome.url} appearance="clean">
+	<Button>
+		<Inline alignV="center" space="sm">
+			<Image src={IconChrome} />
+			add to chrome
+		</Inline>
+	</Button>
+</Link>
