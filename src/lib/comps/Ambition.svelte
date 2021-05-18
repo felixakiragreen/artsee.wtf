3b5d0cad4cee668cea2630657ee06d437cb57fc8
Felix Green <work@felix.green>
2021-05-18 13:54:10 -0400

    build out ambition logo

---
+++ src/lib/comps/Ambition.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+	import { ambition } from '@/lib/datum/socials'
+
+	import Box from '../atoms/Box.svelte'
+	import Anchor from '../atoms/Anchor.svelte'
+	import Image from '../atoms/Image.svelte'
+	import Hexagon from '../bonds/Hexagon.svelte'
+
+	import ambitionLogo from '@/assets/ambition_sharp_flat_clean_empty.svg'
+	import ambitionLogoHover from '@/assets/ambition_sharp_flat_clean.svg'
+
+	const cssBox = {
+		position: 'relative',
+		'#amb-hex-log-hov': {
+			opacity: 0,
+			transition: '$1',
+		},
+		'&:hover': {
+			'#amb-hex-log-hov': {
+				opacity: 1,
+			},
+		},
+	}
+
+	const cssImg = {
+		position: 'absolute',
+		surrounding: 0,
+	}
+</script>
+
+<Anchor url={ambition.url} newTab>
+	<!-- <Box css={{ position: 'absolute', surrounding: '-4px' }}>
+		<Hexagon />
+	</Box> -->
+	<Box css={cssBox}>
+		<Image src={ambitionLogo} alt="Ambition Hexagonal Logo" css={cssImg} />
+		<Image
+			src={ambitionLogoHover}
+			alt="Ambition Hexagonal Logo"
+			css={cssImg}
+			id="amb-hex-log-hov"
+		/>
+
+	</Box>
+</Anchor>
