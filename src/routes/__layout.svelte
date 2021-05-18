64d2979b9e65be67bfed8725b5efab1b02889864
Felix Green <work@felix.green>
2021-05-18 11:59:37 -0400

    stub layout

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,32 @@
+<script>
+	import '$lib/style/global.css'
+	import { globalCss } from '@/ui'
+
+	const globalStyles = globalCss({
+		'#svelte': {
+			visibility: 'visible !important',
+		},
+	})
+
+	globalStyles()
+
+	import Header from './_header.svelte'
+	import Footer from './_footer.svelte'
+</script>
+
+<svelte:head>
+	<title>artsee.wtf</title>
+	<script
+		src="https://cdn.usefathom.com/script.js"
+		data-site=""
+		data-excluded-domains="localhost"
+		defer>
+
+	</script>
+</svelte:head>
+
+<Header />
+
+<slot />
+
+<Footer />
---
+++ src/routes/_header.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { stitch } from '@/ui'
+
+	import Content from '@/lib/bonds/Content.svelte'
+	import Socials from '@/lib/comps/Socials.svelte'
+	import Inline from '@/lib/bonds/Inline.svelte'
+	import Text from '@/lib/bonds/Text.svelte'
+	import Link from '@/lib/bonds/Link.svelte'
+
+	const ss = stitch({
+		position: 'relative',
+		zIndex: 100,
+	})
+</script>
+
+<header class={ss()}>
+	<Content>
+		<Text>
+			new episodes
+			<b>friday @ 09:00 et</b>
+		</Text>
+	</Content>
+</header>
