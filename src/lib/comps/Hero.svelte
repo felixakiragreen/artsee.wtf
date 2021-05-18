4f6b353f53a311a09121ce2fb055e3277d1a143f
Felix Green <work@felix.green>
2021-05-18 16:38:27 -0400

    adjust the shit

--- src/lib/comps/Hero.svelte
+++ src/lib/comps/Hero.svelte
@@ -26,35 +26,55 @@
 	})
 
 	const ss1 = stitch({
-		bg: '$grey200',
+		width: '640px',
+		position: 'relative',
+		left: '$-12',
+
+		'@initial': {
+			width: '0px',
+		},
+		'@sm': {
+			width: '320px',
+		},
+		'@md': {
+			width: '480px',
+		},
+		'@lg': {
+			width: '640px',
+		},
+		// bg: '$grey200',
 		// position: 'relative',
 		// zIndex: 100,
 	})
 </script>
 
 <Box cls={ss}>
-	<Hide below="md">
-		<Box css={{ width: '60vw', position: 'absolute', left: '$-12' }}>
-			<Image src={ImgArtseeTextLarge} css={{ width: '$full' }} />
-		</Box>
+	<Hide below="sm">
+		<!-- <Box css={{ width: '60vw', position: 'absolute', left: '$-12' }}>
+			<Image
+				src={ImgArtseeTextLarge}
+				alt="artsee logo"
+				css={{ maxWidth: '$full', maxHeight: '$full' }}
+			/>
+		</Box> -->
 	</Hide>
 	<Content>
 		<Inline>
 
-			<Stack css={{ width: '60vw' }}>
-				<!-- <Image src={ImgArtseeTextLarge} /> -->
-			</Stack>
+			<Hide below="sm">
+				<Box cls={ss1}>
+					<Image src={ImgArtseeTextLarge} css={{ width: '$full' }} />
+				</Box>
+			</Hide>
 			<Stack align="leading" space="md">
-				<Image src={ImgArtseeGoldLogo} />
+				<Image src={ImgArtseeGoldLogo} alt="artsee logo" />
 				<Text
 					css={{ color: '$gold', textTransform: 'uppercase', fontWeight: '$semi' }}
 				>
 					artsee for chrome
 				</Text>
-				<Text css={{ text: '$xl', fontWeight: '$bold' }}>
-					bring NFTs into your daily life
-				</Text>
-				<Text>
+				<Text mdx="h5">bring NFTs into your daily life</Text>
+				<Text mdx="p">
 					artsee uses your
 					<Image
 						src={IconEth}
