cfa21943ef963d89b0bd8962a36865843db2538f
Felix Green <work@felix.green>
2021-05-18 13:54:58 -0400

    build out footer

---
+++ src/routes/_footer.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { stitch } from '@/ui'
+
+	import Box from '@/lib/atoms/Box.svelte'
+	import Socials from '@/lib/comps/Socials.svelte'
+
+	const ss = stitch({
+		display: 'flex',
+		flexDirection: 'column',
+		// fontFamily: '$main',
+		pt: '$8',
+		sy: '$8',
+	})
+</script>
+
+<footer class={ss()}>
+	<Socials align="center" space="no" />
+
+	<Box css={{ bg: '$grey700', h: '$4' }} />
+</footer>
