b51c9a7ab9287016d81f7b6cc39c56cd997a8728
Felix Green <work@felix.green>
2021-05-18 13:53:07 -0400

    pull out chrome button

---
+++ src/lib/comps/AddToChrome.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import Image from '@/lib/atoms/Image.svelte'
+	import Inline from '@/lib/bonds/Inline.svelte'
+
+	import Button from '@/lib/bonds/Button.svelte'
+	import IconChrome from '@/assets/icon_chrome.svg'
+</script>
+
+<Button>
+	<Inline alignV="center" space="sm">
+		<Image src={IconChrome} />
+		add to chrome
+	</Inline>
+</Button>
