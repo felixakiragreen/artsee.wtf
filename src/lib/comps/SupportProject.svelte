fd1d6fd2219fe3c1991288a12c34760abc9ed363
Felix Green <work@felix.green>
2021-05-19 12:59:30 -0400

    build out support this project

--- src/lib/comps/SupportProject.svelte
+++ src/lib/comps/SupportProject.svelte
@@ -1,4 +1,32 @@
 <script lang="ts">
-	// TODO: make it popdown
-	// (send NFTs or ETH to artsee.eth, to see it featured)
+	import { stitch } from '@/ui'
+
+	import Box from '@/lib/atoms/Box.svelte'
+	import Text from '@/lib/bonds/Text.svelte'
+	import Link from '@/lib/bonds/Link.svelte'
+
+	const ss = stitch({
+		bg: '$grey200',
+		fontFamily: '$main',
+	})
+
+	let show = false
+	function toggle() {
+		show = !show
+	}
 </script>
+
+<Box css={{ position: 'relative' }}>
+	<div on:click={toggle}>
+		<Link css={{ text: '$sm' }}>support this project :)</Link>
+	</div>
+	{#if show}
+		<Box css={{ position: 'absolute', w: '$xl' }}>
+			<Text>just send Eth to artsee.eth 🙏</Text>
+			<br />
+			<Text css={{ text: '$xs' }}>
+				(or NFTs and they'll show up in the public gallery!)
+			</Text>
+		</Box>
+	{/if}
+</Box>
