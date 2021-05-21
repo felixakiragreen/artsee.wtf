0b708c4481a78df47a5d9d391bc279ecfe13566d
Felix Green <work@felix.green>
2021-05-20 23:05:05 -0400

    add ambition to the footer

--- src/routes/_footer.svelte
+++ src/routes/_footer.svelte
@@ -2,7 +2,9 @@
 	import { stitch } from '@/ui'
 
 	import Box from '@/lib/atoms/Box.svelte'
+	import Inline from '@/lib/bonds/Inline.svelte'
 	import Socials from '@/lib/comps/Socials.svelte'
+	import Ambition from '@/lib/comps/Ambition.svelte'
 
 	const ss = stitch({
 		display: 'flex',
@@ -13,7 +15,10 @@
 </script>
 
 <footer class={ss()}>
-	<Socials align="center" space="no" />
+	<Inline align="center">
+		<Ambition />
+		<Socials space="no" />
+	</Inline>
 
 	<Box css={{ bg: '$grey700', h: '$4' }} />
 </footer>
